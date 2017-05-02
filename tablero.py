aa0364d62860e78ce8b367a2c85048b48ffcd61e
Jumaroag <jumaroag@gmail.com>
2017-05-02 18:25:21 -0300

    Cambio el nombre de la funcion "tablero_1" a "posciciones_del_tablero"

--- tablero.py
+++ tablero.py
@@ -1,4 +1,4 @@
-def tablero_1():
+def posciciones_del_tablero():
     tablero_1 = {"a1": "o","a2": "o","a3": ".","a4": "o","a5": "o",
                  "b1": "o","b2": ".","b3": "o","b4": ".","b5": "o",
                  "c1": ".","c2": "o","c3": "o","c4": "o","c5": ".",
