50b2967268c00ede3a332f73954c46386d4fd1b0
Jumaroag <jumaroag@gmail.com>
2017-05-02 18:26:02 -0300

    Pequeñas correciones de la función

--- traductor.py
+++ traductor.py
@@ -1,13 +1,13 @@
 import tablero
-tablero.tablero_1()
+tablero.posciciones_del_tablero()
 def traductor():
     print("¿Qué poscición deseas jugar?")
     a = input()
     a = str(a)
     a = a.lower()
-    if (a in tablero.tablero_1()):
-        print(tablero.tablero_1()[a])
-    elif (a not in tablero.tablero_1()):
+    if (a in tablero.posciciones_del_tablero()):
+        print(tablero.posciciones_del_tablero()[a])
+    elif (a not in tablero.posciciones_del_tablero()):
         print("Poscicion no valida, elija una poscición comprendida entre A1 y E5")
         traductor()
 
